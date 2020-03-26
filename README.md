515decc73f08dbe5c095a3827accf9815d1b752b
John MacGregor <jmacgregor@pillartechnology.com>
2020-03-26 15:27:19 -0400

    Extract bit stuffs to methods documented so Chris can tell me how badly I understand this

--- README.md
+++ README.md
@@ -7,15 +7,15 @@ Pay attention to the hardware and software requirements listed in the [README](h
 ## To Compile
 
 ```sh
-avr-gcc -c -Wall -Os -mmcu=atmega328p -DF_CPU=16000000UL hello-world.c -o hello-world.o
+avr-gcc -c -Wall -Os -mmcu=atmega328p -DF_CPU=16000000UL hello-world.c -o bin/hello-world.o
 
-avr-gcc -Wall -Os -mmcu=atmega328p -DF_CPU=16000000UL hello-world.o -o hello-world.elf
+avr-gcc -Wall -Os -mmcu=atmega328p -DF_CPU=16000000UL bin/hello-world.o -o bin/hello-world.elf
 
-avr-objcopy -j .text -j .data -O ihex hello-world.elf hello-world.hex
+avr-objcopy -j .text -j .data -O ihex bin/hello-world.elf bin/hello-world.hex
 ```
 
 ## To Flash to Board
 
 ```sh
-avrdude -v -patmega328p -carduino -b115200 -D -Uflash:w:hello-world.hex:i
+avrdude -v -patmega328p -carduino -b115200 -D -Uflash:w:bin/hello-world.hex:i
 ```
---
+++ hello-world.c
@@ -0,0 +1,38 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+// Shift 3rd(?) bit on the port data direction register to set it as output
+void setup_onboard_led_as_output()
+{
+    DDRB |= (1 << DDB5);
+}
+
+// Shift 5th bit to power the port
+void onboard_led_on()
+{
+    PORTB |= (1 << PORTB5);
+}
+
+// I actually have no idea what the & is doing, exactly
+// right right-hand side is shifting the 5th bit in the opposite 
+// direction as before
+// but instead of piping that to make an assignment, we make the 
+// port = 0 because current value of portb and the ~'d result of the 
+// 5th bit are not equal
+void onboard_led_off()
+{
+    PORTB &= ~(1 << PORTB5);
+}
+
+int main(void)
+{
+    setup_onboard_led_as_output();
+
+    for(;;)
+    {
+        onboard_led_on();
+        _delay_ms(500);
+        onboard_led_off();
+        _delay_ms(500);
+    }
+}

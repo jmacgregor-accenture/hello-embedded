c3a536e0f879a8ba39b3840ad831b8fa142c22fe
John MacGregor <jmacgregor@pillartechnology.com>
2020-04-16 14:28:11 -0400

    Remove unnecessary complexity; toggle LED directly

--- timer-blink/timed-blink.c
+++ timer-blink/timed-blink.c
@@ -5,87 +5,28 @@
 bool light_on;
 
 // Shift the passed bit on the port data direction register to set it as output
-void setup_bit_as_output(int bit)
+void setup_onboard_led_as_output()
 {
-    if (bit == 5)
-    {
-        DDRB |= (1 << DDB5);
-    }
-    else if (bit == 4)
-    {
-        DDRB |= (1 << DDB4);
-    }
-    else if (bit == 2)
-    {
-        DDRB |= (1 << DDB4);
-    }
+    DDRB |= (1 << DDB5);
 }
 
-// Take the current bit values of PORTB, then get the complement of shifting the passed bit.
-// Then, use the bitwise AND to assign a new value to PORTB
-// Bitwise AND combined with the complement value will leave all current 1 values unchanged but set all others to 0
-void switch_power_off(int bit)
+void toggle_led()
 {
-    if (bit == 5)
-    {
-        PORTB &= ~(1 << PORTB5);
-    }
-    else if (bit == 4)
-    {
-        PORTB &= ~(1 << PORTB4);
-    }
-    else if (bit == 2)
-    {
-        PORTB &= ~(1 << PORTB2);
-    }
-
-    light_on = false;
-}
-
-// Shift the passed bit to power the port
-void switch_power_on(int bit)
-{
-    if (bit == 5)
-    {
-        PORTB |= (1 << PORTB5);
-    }
-    else if (bit == 4)
-    {
-        PORTB |= (1 << PORTB4);
-    }
-    else if (bit == 2)
-    {
-        PORTB |= (1 << PORTB2);
-    }
-
-    light_on = true;
-}
-
-void toggle_led(int bit)
-{
-    if (light_on)
-    {
-        switch_power_off(bit);
-    }
-    else
-    {
-        switch_power_on(bit);
-    }
-    
+    PORTB ^= (1 << PORTB5);
 }
 
 int main(void)
 {
-    setup_bit_as_output(5);
+    setup_onboard_led_as_output();
 
-    TCCR1B |= (1 << CS10); // How do we know to use this clock source??
+    TCCR1B |= (1 << CS10);
 
     for(;;)
     {
 
         if (TCNT1 >= 49999)
         {
-            toggle_led(5);
+            toggle_led();
             TCNT1 = 0;
         }
     }
